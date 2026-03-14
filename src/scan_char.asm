d0843a94c4cc9227ea1f190f481fc4067d013d40
CAMartínez Méndez <129247877+TercerPersonaje@users.noreply.github.com>
2026-03-14 17:28:47 -0600

    Completar función scan_char para leer un carácter
    Se implementó la función scan_char utilizando la syscall read para
    leer un byte desde stdin. El carácter leído se almacena en memoria
    y se devuelve en el registro AL.

--- src/scan_char.asm
+++ src/scan_char.asm
@@ -17,8 +17,16 @@ scan_char:
 
     ; TODO:
     ; 1. usar syscall read
+    mov eax, 3
+
     ; 2. leer 1 byte desde stdin
+    mov ebx, 0
+    mov ecx, char_buffer
+    mov edx, 1
+    int 0x80
+
     ; 3. devolverlo en AL
+    mov al, [char_buffers]
 
     mov esp, ebp
     pop ebp
