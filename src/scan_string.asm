9b5d0863eacdf1e4a2e1eec030c38956161e0b4d
CAMartínez Méndez <129247877+TercerPersonaje@users.noreply.github.com>
2026-03-14 17:38:41 -0600

    Completar función scan_string para leer cadenas desde stdin
    Se agregó la llamada a la syscall read para leer una cadena desde stdin y se implementó la lógica para reemplazar el salto de línea por el terminador nulo, permitiendo que la cadena pueda usarse con print_string.

--- src/scan_string.asm
+++ src/scan_string.asm
@@ -13,11 +13,31 @@ scan_string:
     push ebp
     mov ebp, esp
 
+    mov ecx, eax
+    mov edx, ebx
+
     ; TODO:
     ; 1. syscall read
+    mov eax, 3
+    mov ebx, 0
+    int 0x80
+ 
     ; 2. guardar en buffer
-    ; 3. agregar terminador 0
+    mov esi, ecx
+    add esi, eax
+    dec esi
 
+    ; 3. agregar terminador 0
+    cmp byte [esi], 10
+    jne .add_null
+
+    mov byte [esi], 0
+    jmp .done
+
+.add_null:
+    mov byte [esi+1], 0
+
+.done:
     mov esp, ebp
     pop ebp
     ret
